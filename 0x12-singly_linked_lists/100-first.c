91c4b6fdb13ba368ee8c507b916be1cd2047b245
kwenziwa <kwenziwa@live.com>
2023-03-30 06:01:46 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - executes before the main
+ *
+ * Return: the print out.
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
