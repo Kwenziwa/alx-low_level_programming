ba068209dbf9f3fc52efa5334e67810706bd2582
kwenziwa <kwenziwa@live.com>
2023-05-22 14:15:58 -0700

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -padantic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRAY_PATH=.:$LD_LIBRARY_PATH
